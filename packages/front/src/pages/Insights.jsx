884df8e269889c85b156f0b4006e4f0ff84e3338
clementjuventin <clementjuventin@gmail.com>
2022-10-29 19:24:42 +0200

    feat: form inisghts start

--- packages/front/src/pages/Insights.jsx
+++ packages/front/src/pages/Insights.jsx
@@ -1,3 +1,8 @@
+import { InputGroup, InputLeftElement, Input, Container } from '@chakra-ui/react'
+import { ArrowRightIcon } from '@chakra-ui/icons'
+
+import '../css/Insights.css'
+
 const Insights = (props) => {
   return (
     <>
@@ -9,6 +14,38 @@ const Insights = (props) => {
           <li>Snap install</li>
         </ul>
       </p>
+      <Container className='formContainer' maxW='md'>
+        <InputGroup>
+          <InputLeftElement
+            pointerEvents='none'
+            color='gray.300'
+            fontSize='1.2em'
+          // children={<LinkIcon color='green.500' />}
+          />
+          <Input placeholder='To' />
+        </InputGroup>
+        <InputGroup>
+          <InputLeftElement
+            pointerEvents='none'
+            color='gray.300'
+            fontSize='1.2em'
+          // children={<LinkIcon color='green.500' />}
+          />
+          <Input placeholder='From' />
+        </InputGroup>
+        <InputGroup>
+          <InputLeftElement
+            pointerEvents='none'
+            color='gray.300'
+            fontSize='1.2em'
+          // children={<LinkIcon color='green.500' />}
+          />
+          <Input placeholder='Input data' />
+        </InputGroup>
+        <Button rightIcon={<ArrowRightIcon />} colorScheme='blue' variant='outline'>
+          Submit
+        </Button>
+      </Container>
     </>
   )
 };
