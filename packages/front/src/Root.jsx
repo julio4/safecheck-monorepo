e110791e7fffe83c1e9b7564d66a9a228f0ce915
quintenbons <quintenbons@outlook.com>
2022-10-29 13:17:08 +0200

    join

--- packages/front/src/Root.jsx
+++ packages/front/src/Root.jsx
@@ -1,3 +1,7 @@
-const Root = () => <h1>ROOT</h1>;
+import './css/Root.css'
+
+const Root = () => {
+  return <h1>ROOT</h1>
+}
 
 export default Root;
---
+++ packages/front/src/components/Header.jsx
@@ -0,0 +1,8 @@
+import '../css/Header.css';
+const Header = (props => {
+  return (
+    <header className='top-bar'>
+      <h1>SafeCheck</h1>
+    </header>
+  );
+});
