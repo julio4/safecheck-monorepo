c910c12ffe24010eda726b9c826035eb998eb3fa
Gwen <gwenole19@gmail.com>
2022-10-30 03:59:25 +0000

    404 (in progress)

--- packages/front/src/pages/NotFound.jsx
+++ packages/front/src/pages/NotFound.jsx
@@ -1,5 +1,19 @@
+//import "../css/NotFound.css";
+
+//import ErrorDesign from '../assets/404.svg';
+
 const NotFound = (props) => {
-  return <h1>404 not found</h1>
+  return (
+    <a  target="_blank" href="https://codepen.io/uiswarup/full/KKRaNPm">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-12 col-md-12 mt-5 mb-5">
+            <img src={require("../assets/404.png")} alt={"error_img"}/>
+          </div>
+        </div>
+      </div>
+    </a>
+  )
 };
 
 export default NotFound;
