590c0778ae8d82b977a817967d331b621520c225
julio4 <jules.doumeche@gmail.com>
2022-10-30 05:50:16 +0000

    update snap

--- packages/snap/src/index.ts
+++ packages/snap/src/index.ts
@@ -2,5 +2,8 @@ import { OnTransactionHandler } from "./types"
 import { getInsights } from "./insights"
 
 export const onTransaction: OnTransactionHandler = async ({ transaction, chainId }) => {
-  return await getInsights(transaction)
+  // We only support ethereum mainnet for now
+  // We could add support with chainId
+  const insights_data = await getInsights(transaction)
+  return { insights: insights_data }
 };
